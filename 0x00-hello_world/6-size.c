681b955bdeef94985b8ee7aa7c0942c25fb4e622
ErmiMe <emessele@gmail.com>
2022-09-13 12:10:04 -0700

    Remove T6 spaces after sizeof

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,10 +12,10 @@ int main(void)
 	long long int lli;
 	float f;
 
-	printf("Size of a char: %d byte(s)\n", (int)sizeof (c));
-	printf("Size of an int: %d byte(s)\n", (int)sizeof (i));
-	printf("Size of a long int: %d byte(s)\n", (int)sizeof (li));
-	printf("Size of a long long int: %d byte(s)\n", (int)sizeof (lli));
-	printf("Size of a float: %d byte(s)\n", (int)sizeof (f));
+	printf("Size of a char: %d byte(s)\n", (int)sizeof(c));
+	printf("Size of an int: %d byte(s)\n", (int)sizeof(i));
+	printf("Size of a long int: %d byte(s)\n", (int)sizeof(li));
+	printf("Size of a long long int: %d byte(s)\n", (int)sizeof(lli));
+	printf("Size of a float: %d byte(s)\n", (int)sizeof(f));
 	return (0);
 }
