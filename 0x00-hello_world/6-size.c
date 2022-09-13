bb4b49a8611340d99f15f75464567fcda306b9dc
ErmiMe <emessele@gmail.com>
2022-09-13 11:17:37 -0700

    Correct T6 by removing $ from ahead of variables in sizeof

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,10 +12,10 @@ int main(void)
 	long long int lli;
 	float f;
 
-	put("Size of a char: " (sizeof $c));
-	put("Size of an int: " (sizeof $i));
-	put("Size of a long int: " (sizeof $li));
-	put("Size of a long long int: " (sizeof $lli));
-	put("Size of a float: " (sizeof $f));
+	put("Size of a char: " (sizeof c));
+	put("Size of an int: " (sizeof i));
+	put("Size of a long int: " (sizeof li));
+	put("Size of a long long int: " (sizeof lli));
+	put("Size of a float: " (sizeof f));
 	return (0);
 }
