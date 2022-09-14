f09db58d90fcf5c30f81df297c8e0bc33271eed9
ErmiMe <emessele@gmail.com>
2022-09-14 11:40:39 -0700

    Add T5 for checker

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - lists 0-9
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i = 0;
+	int str[11];
+
+	while (i <= 9)
+	{
+		putchar(str[i]);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
