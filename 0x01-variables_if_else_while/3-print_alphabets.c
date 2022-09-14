7e11247585cb2eacc907d9e50d8c0303221f2d4b
ErmiMe <emessele@gmail.com>
2022-09-14 11:15:47 -0700

    Add T3 for checker

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * main - prints every letter of the alphabet
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i = 'a';
+
+	while (i <= 'z')
+	{
+		putchar(i);
+		i++;
+	}
+
+	i = 'A';
+
+	while (i <= 'Z')
+	{
+		putchar(i);
+		i++;
+	}
+
+	putchar('\n');
+
+	return (0);
+}
