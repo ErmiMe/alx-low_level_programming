54a0830ada70aa4b3171d29c3fb715719acd5c3c
ErmiMe <emessele@gmail.com>
2022-09-16 06:26:38 -0700

    0x02 T8 Adds for checker. T7 paused.

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * jack_bauer - prints every minute of the day
+ * @hour: hour var
+ * @minute: minute var
+ * Return: Always 0
+ */
+
+void jack_bauer(void);
+{
+	int hour = 0
+	int minute = 0
+
+	while (hour <= 24)
+	{
+		while (minute <= 60)
+		{
+			_putchar("%02d", hour);
+			_putchar("%02d", minute);
+			_outchar("\n");
+
+			minute++;
+		}
+
+		hour++;
+	}
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -6,3 +6,28 @@ int _isalpha(int c);
 int print_sign(int n);
 int _abs(int);
 int print_last_digit(int);
+
+
+#include <unistd.h>
+
+/**
+ *
+ *  * _putchar - writes the character c to stdout
+ *
+ *   * @c: The character to print
+ *
+ *    *
+ *
+ *     * Return: On success 1.
+ *
+ *      * On error, -1 is returned, and errno is set appropriately.
+ *
+ *       */
+
+int _putchar(char c)
+
+{
+
+		return (write(1, &c, 1));
+
+}
