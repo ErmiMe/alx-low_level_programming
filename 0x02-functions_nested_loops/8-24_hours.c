9dd4fd71cd89e032a164785c00adfed79d20f080
ErmiMe <emessele@gmail.com>
2022-09-16 07:52:53 -0700

    0x02 T8 change to 2 while loops only

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -12,53 +12,20 @@ void jack_bauer(void)
 	int hour = 0;
 	int minute = 0;
 
-	while (hour < 10)
+	while (hour <= 24)
 	{
-		while (minute < 10)
+		while (minute <= 60)
 		{
-			_putchar('0' + hour);
+			_putchar((hour / 10) + '0');
+			_putchar((hour % 10) + '0');
 			_putchar(':');
-			_putchar('0' + minute);
-			_putchar('\n');
-
-			minute++;
-		}
-
-		while (minute <=60)
-		{
-			_putchar('0' + hour);
-			_putchar(':');
-			_putchar(minute);
+			_putchar((minute / 10) + '0');
+			_putchar((minute / 10) + '0');
 			_putchar('\n');
 
 			minute++;
 		}
 
 		hour++;
-	}
-
-	while (hour <= 60)
-	{
-		while (minute < 10)
-		{
-			_putchar(hour);
-			_putchar(':');
-			_putchar('0' + minute);
-			_putchar('\n');
-
-			minute++;
-		}
-
-		while (minute <=60)
-		{
-			_putchar(hour);
-			_putchar(':');
-			_putchar(minute);
-			_putchar('\n');
-
-			minute++;
-		}
-
-		hour ++;
 	}
 }
