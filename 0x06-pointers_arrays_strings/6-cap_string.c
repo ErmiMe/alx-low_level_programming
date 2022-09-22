f1512e3c7affbed68d571c33969d26fd7eb2d54b
ErmiMe <emessele@gmail.com>
2022-09-22 15:52:52 -0700

    0x06 T6-7 add for check

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * cap_string - change the first character to capital
+ * @str: string to be changed
+ *
+ * Return: a string
+ */
+char *cap_string(char *str)
+{
+	int i = 0;
+
+	while (str[++i])
+	{
+		while (!(str[i] >= 'a' && str[i] <= 'z'))
+			i++;
+		if (str[i - 1] == ' ' ||
+				str[i - 1] == '\t' ||
+				str[i - 1] == '\n' ||
+				str[i - 1] == ',' ||
+				str[i - 1] == ';' ||
+				str[i - 1] == '.' ||
+				str[i - 1] == '!' ||
+				str[i - 1] == '?' ||
+				str[i - 1] == '"' ||
+				str[i - 1] == '(' ||
+				str[i - 1] == ')' ||
+				str[i - 1] == '{' ||
+				str[i - 1] == '}')
+			str[i] -= 32;
+	}
+	return (str);
+}
