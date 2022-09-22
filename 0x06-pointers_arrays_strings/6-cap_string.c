bd69704b9de2ff35b318a68819c84bc15ec53703
ErmiMe <emessele@gmail.com>
2022-09-22 16:14:09 -0700

    0x06 cmon 6 and 7

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,17 +1,16 @@
 #include "main.h"
 
 /**
- * cap_string - capitalize the first character
+ * cap_string - change the first character to capital
  * @str: string to be changed
  *
  * Return: a string
  */
-
 char *cap_string(char *str)
 {
 	int i = 0;
 
-	while (str[i])
+	while (str[++i])
 	{
 		while (!(str[i] >= 'a' && str[i] <= 'z'))
 			i++;
